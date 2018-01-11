b04d95f32c5f36b12b96fc75ab6e0e366c09e7ca
Michael Palermo <michaelpalermo@Michaels-MacBook-Pro-2.local>
2018-01-11 16:58:21 -0700

    footer component

---
+++ client/src/components/Footer/footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer w-100 bg-dark text-white align-content-around">
+          <ul className="nav justify-content-center">
+            <li className="nav-item">
+              <a className="nav-link" href="/leaderboard">Leaderboard</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/game">New Game</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/API">API</a>
+            </li>
+          </ul>    
+    </div>
+  )
+}
+
+
+export default Footer;
