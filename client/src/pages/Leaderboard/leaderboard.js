96cfff1999b552f4958faa6024e5392206e6d4d9
janisnjohn <janisnjohn@gmail.com>
2018-01-12 13:21:10 -0700

    leaderboard working and styled

--- client/src/pages/Leaderboard/leaderboard.js
+++ client/src/pages/Leaderboard/leaderboard.js
@@ -5,16 +5,13 @@ import { Col, Row, Container } from "../../components/Grid";
 import BackGround from "../../components/BackGround";
 import API from "../../utils/API";
 import './leaderboard.css';
-
 class Leaderboard extends Component {
   state = {
-    leaders: null,
+    leaders: [],
   };
-
   componentDidMount() {
       this.getLeaders();
   }
-
   getLeaders() {
     API.getLeaders()
     .then( response => {
@@ -25,20 +22,40 @@ class Leaderboard extends Component {
       })
   }
   render() {
+    // ********** The way you did it is totally fine, just wanting to show you the different routes you can go ***********
+
+
+    // There is no need for this line since we can handle it in the render
+    /* const leaderList = this.state.leaders.map((list) =>
+    //   <li>{list}</li>
+    );*/   
     return (
       <Container fluid>
-        <Row>
-          <Col size="md-12">
-          <article>
+        <BackGround />
+        <center>
+          <header id="showcase">
             <h1>Leaderboard</h1>
-            <div>{JSON.stringify(this.state.leaders)}</div>
-          </article>
-            <BackGround />  
-          </Col>
-        </Row>
+          </header>
+
+            <table id="highScore" class="highlight-table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Score</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    { this.state.leaders.length > 0 &&
+                    /*Map over our array */
+                    this.state.leaders.map((list, i) => 
+                      <tr><td>{list.pilot}</td><td>{list.score}</td></tr>)
+                    }
+                </tbody>
+            </table>
+        </center>
       </Container>
     );
   }
 }
-
 export default Leaderboard;
